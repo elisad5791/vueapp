09074ac8c3648ec84e20974cbbb512c0aa128427
elisad5791 <elisad5791@mail.ru>
2025-08-08 22:00:53 +0500

    Add persons app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,51 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { ref } from "vue";
+
+const persons = ref([
+  { id: 1, name: "John", age: 35 },
+  { id: 2, name: "Jane", age: 33 },
+  { id: 3, name: "Helen", age: 31 },
+  { id: 4, name: "Peter", age: 40 },
+]);
+
+const showList = ref(false);
+const showDetails = ref(false);
+const color = ref('#000000');
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <h1>Persons</h1>
+
+  <button type="button" @click="showList = !showList">
+    {{ showList ? "Hide" : "Show" }} All
+  </button>
+
+  <div v-if="showList">
+    <ol>
+      <li v-for="person in persons" :key="person.id">
+        {{ person.name }}
+        <span v-show="showDetails">({{ person.age }} years)</span>
+      </li>
+    </ol>
+
+    <div class="settings">
+      <span>Color</span>
+      <input type="color" v-model="color" />
+
+      <button type="button" @click="showDetails = !showDetails">
+        {{ showDetails ? "Hide" : "Show" }} details
+      </button>
+    </div>
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+.settings {
+  display: flex;
+  gap: 10px;
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+
+li:hover {
+  color: v-bind(color);
 }
 </style>
