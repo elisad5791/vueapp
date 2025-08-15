9a76aa4f4690adae2f75e596c3b10b2778791df2
elisad5791 <elisad5791@mail.ru>
2025-08-16 00:54:36 +0500

    Add products app

---
+++ src/components/ProductItem.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps(["product"]);
+</script>
+
+<template>
+  <div class="card bg-base-100 w-96 shadow-sm mx-auto">
+    <figure>
+      <img class="w-full h-96 object-contain" :src="product.image" :alt="product.title" />
+    </figure>
+    <div class="card-body flex-none">
+      <h2 class="card-title">
+        {{ product.title }}
+        <div class="badge badge-secondary">{{ product.rating.rate }}</div>
+      </h2>
+      <p class="italic">{{ product.category }}</p>
+      <p>
+        {{ product.description }}
+      </p>
+      <div class="card-actions justify-end">
+        <div class="badge badge-success">${{ product.price }}</div>
+        <div class="badge badge-outline">{{ product.rating.count }} voices</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,16 @@
+<script setup>
+import ProductItem from './ProductItem.vue';
+
+const props = defineProps(['products'])
+</script>
+
+<template>
+  <h1 class="text-4xl font-bold mt-4 text-center">Products</h1>
+
+  <div class="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 py-12 md:max-w-200 xl:max-w-300 mx-auto">
+    <ProductItem v-for="product in products" :key="product.id" :product="product" />
+  </div>
+</template>
+
+<style scoped>
+</style>
